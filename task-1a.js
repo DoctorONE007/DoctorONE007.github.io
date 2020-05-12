be9031a681c3165d9109b84c5ad6d2ac4084a472
DoctorONE007 <43263493+DoctorONE007@users.noreply.github.com>
2020-05-12 12:18:16 +0300

    Update task-1a.js

--- task-1a.js
+++ task-1a.js
@@ -225,7 +225,9 @@ form.onsubmit = function(evt) {
 		`;
 
 
-        
+        let div = document.createElement('div');
+div.className = "alert";
+div.innerHTML = "<strong>Всем привет!</strong> Вы прочитали важное сообщение.";
 
 
 
